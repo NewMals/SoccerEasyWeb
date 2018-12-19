c662130c4e406ef6dc05a8d831ea6b78de5bdb78
Michael Andres <Michael Andres Lozano Sanchez>
2018-12-19 18:05:16 -0500

    Creacion de la pagina de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'Login', loadChildren: './pages/login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
